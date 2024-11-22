407733ac9441cb5f1b6fd49f94688fb5f1a6c345
SURAJ SINGH <rameshwarsingh99351@gmail.com>
2024-11-22 16:42:27 +0530

    Check Commit

---
+++ Basic Program/sum.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class sum
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a, b , sum = 0;
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        sum = a + b;
+
+        System.out.println("Sum of the number is = " + sum);
+    }
+}
